e918faba191849e75d24fdc2b09d365e6d9b6bf9
websearch <websearch@4b1de6bc-0fff-0410-8a84-d09add9403b5>
2014-07-29 08:19:23 +0000

    = SnmpPlatform =  * 메모리 누수에 대한 소스 코드 출력 기능을 추가함  * TCP 라이브러리를 추가함

--- trunk/SnmpPlatform/SnmpUdp.h
+++ trunk/SnmpPlatform/SnmpUdp.h
@@ -16,8 +16,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
  */
 
-#ifndef _SIP_UDP_H_
-#define _SIP_UDP_H_
+#ifndef _SNMP_UDP_H_
+#define _SNMP_UDP_H_
 
 #include <string>
 
