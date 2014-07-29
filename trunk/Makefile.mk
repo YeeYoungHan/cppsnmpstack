5d6676cf836545b24caf44949253f354e5f26538
websearch <websearch@4b1de6bc-0fff-0410-8a84-d09add9403b5>
2014-07-29 09:07:29 +0000

    리눅스에서 빌드할 수 있는 Makefile 을 생성함

---
+++ trunk/Makefile.mk
@@ -0,0 +1,9 @@
+# Makefile for all of C++ SnmpStack
+#
+# programmer : yee young han ( websearch@naver.com )
+#            : http://blog.naver.com/websearch
+# start date : 2014/07/29
+
+CC=g++
+CFLAGS=-Wall -O2 -D_REENTRANT -g #-pg
+
