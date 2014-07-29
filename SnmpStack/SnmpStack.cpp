3c7e813d155ca0e8f9b919f6249b0d5329026af6
websearch <websearch@4b1de6bc-0fff-0410-8a84-d09add9403b5>
2014-07-29 08:11:11 +0000

    = SnmpStack =  * CSnmpStack 에 static SendRequest 메소드를 추가함

--- SnmpStack/SnmpStack.cpp
+++ SnmpStack/SnmpStack.cpp
@@ -17,6 +17,8 @@
  */
 
 #include "SnmpStack.h"
+#include "SnmpUdp.h"
+#include "Log.h"
 
 CSnmpStack::CSnmpStack()
 {
@@ -24,4 +26,51 @@ CSnmpStack::CSnmpStack()
 
 CSnmpStack::~CSnmpStack()
 {
+}
+
+bool CSnmpStack::SendRequest( const char * pszDestIp, int iPort, CSnmpMessage & clsRequest, CSnmpMessage & clsResponse )
+{
+	char szPacket[1500], szIp[16];
+	int  iPacketLen;
+	uint16_t	sPort;
+	bool bRes = false;
+	Socket hSocket = UdpSocket();
+	if( hSocket == INVALID_SOCKET )
+	{
+		CLog::Print( LOG_ERROR, "%s UdpSocket error", __FUNCTION__ );
+		return false;
+	}
+
+	iPacketLen = clsRequest.MakePacket( szPacket, sizeof(szPacket) );
+	if( iPacketLen == -1 )
+	{
+		CLog::Print( LOG_ERROR, "%s clsRequest.MakePacket error", __FUNCTION__ );
+		goto FUNC_END;
+	}
+
+	if( UdpSend( hSocket, szPacket, iPacketLen, pszDestIp, iPort ) == false )
+	{
+		CLog::Print( LOG_ERROR, "%s UdpSend error(%d)", __FUNCTION__, GetError() );
+		goto FUNC_END;
+	}
+
+	iPacketLen = sizeof(szPacket);
+	if( UdpRecv( hSocket, (char *)szPacket, &iPacketLen, szIp, sizeof(szIp), &sPort ) == false )
+	{
+		CLog::Print( LOG_ERROR, "%s UdpRecv error(%d)", __FUNCTION__, GetError() );
+		goto FUNC_END;
+	}
+
+	if( clsResponse.ParsePacket( szPacket, iPacketLen ) == -1 )
+	{
+		CLog::Print( LOG_ERROR, "%s clsResponse.ParsePacket error(%d)", __FUNCTION__, GetError() );
+		goto FUNC_END;
+	}
+
+	bRes = true;
+
+FUNC_END:
+	closesocket( hSocket );
+
+	return bRes;
 }
