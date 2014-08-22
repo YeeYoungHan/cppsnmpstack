0f97e214718d8c28f85b8884795fd25093e475d3
websearch <websearch@4b1de6bc-0fff-0410-8a84-d09add9403b5>
2014-08-22 07:20:03 +0000

    = SnmpStack =  * 동기 방식으로 실행되는 CSnmpSession 클래스를 추가함

---
+++ SnmpStack/SnmpSession.cpp
@@ -0,0 +1,150 @@
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#include "SnmpSession.h"
+#include "AsnNull.h"
+#include "Log.h"
+#include "MemoryDebug.h"
+
+#include "SnmpSessionPrivate.hpp"
+
+CSnmpSession::CSnmpSession() : m_iPort(161), m_iIp(0), m_sPort(0)
+	, m_iMiliTimeout(1000), m_iReSendCount(5), m_iRequestId(0)
+	, m_hSocket(INVALID_SOCKET)
+{
+}
+
+CSnmpSession::~CSnmpSession()
+{
+	Close();
+}
+
+bool CSnmpSession::SetDestination( const char * pszIp, int iPort )
+{
+	if( pszIp == NULL || strlen(pszIp) == 0 ) return false;
+	if( iPort <= 0 || iPort > 65535 ) return false;
+
+	m_strIp = pszIp;
+	m_iPort = iPort;
+
+	m_iIp = inet_addr( pszIp );
+	m_sPort = htons( m_iPort );
+
+	return true;
+}
+
+bool CSnmpSession::SetSnmpv2( const char * pszCommunity )
+{
+	if( pszCommunity == NULL ) return false;
+
+	m_strCommunity = pszCommunity;
+
+	return true;
+}
+
+bool CSnmpSession::SetSnmpv3( const char * pszUserName, const char * pszAuthPassWord, const char * pszPrivPassWord )
+{
+	if( pszUserName == NULL ) return false;
+	if( pszAuthPassWord == NULL ) return false;
+
+	m_strUserName = pszUserName;
+	m_strAuthPassWord = pszAuthPassWord;
+
+	if( pszPrivPassWord )
+	{
+		m_strPrivPassWord = pszPrivPassWord;
+	}
+
+	return true;
+}
+
+bool CSnmpSession::SetTimeout( int iMiliSecond )
+{
+	if( iMiliSecond <= 0 ) return false;
+
+	m_iMiliTimeout = iMiliSecond;
+
+	return true;
+}
+
+bool CSnmpSession::SetReSendCount( int iReSendCount )
+{
+	if( iReSendCount < 0 ) return false;
+
+	m_iReSendCount = iReSendCount;
+
+	return true;
+}
+
+bool CSnmpSession::Open()
+{
+	if( m_hSocket != INVALID_SOCKET ) return false;
+
+	m_hSocket = UdpSocket();
+	if( m_hSocket == INVALID_SOCKET ) return false;
+
+	return true;
+}
+
+bool CSnmpSession::Close()
+{
+	if( m_hSocket != INVALID_SOCKET )
+	{
+		closesocket( m_hSocket );
+		m_hSocket = INVALID_SOCKET;
+	}
+
+	return true;
+}
+
+bool CSnmpSession::SendRequest( CSnmpMessage * pclsRequest, CSnmpMessage * pclsResponse )
+{
+	if( SendRecv( pclsRequest, pclsResponse ) == false ) return false;
+
+	if( pclsRequest->m_cMsgFlags == SNMP_MSG_FLAG_REPORT )
+	{
+		CSnmpMessage * pclsSecondRequest = CSnmpMessage::Create( pclsRequest );
+		if( pclsSecondRequest )
+		{
+			pclsSecondRequest->m_iMsgId = ++m_iRequestId;
+			pclsSecondRequest->m_iRequestId = pclsSecondRequest->m_iMsgId;
+			pclsSecondRequest->m_strOid = pclsSecondRequest->m_strReqOid;
+
+			pclsSecondRequest->m_strMsgAuthEngineId = pclsResponse->m_strMsgAuthEngineId;
+			pclsSecondRequest->m_iMsgAuthEngineBoots = pclsResponse->m_iMsgAuthEngineBoots;
+			pclsSecondRequest->m_iMsgAuthEngineTime = pclsResponse->m_iMsgAuthEngineTime;
+			pclsSecondRequest->m_strMsgUserName = pclsSecondRequest->m_strUserId;
+
+			pclsSecondRequest->m_strContextEngineId = pclsResponse->m_strContextEngineId;
+			pclsSecondRequest->m_pclsValue = new CAsnNull();
+
+			pclsSecondRequest->SetPrivParams( );
+			pclsSecondRequest->SetAuthParams( );
+
+			if( SendRecv( pclsRequest, pclsResponse ) == false ) return false;
+
+			if( pclsResponse->m_strEncryptedPdu.empty() == false )
+			{
+				pclsResponse->m_strPrivPassWord = pclsRequest->m_strPrivPassWord;
+				pclsResponse->ParseEncryptedPdu( );
+			}
+		}
+	}
+
+	return true;
+}
